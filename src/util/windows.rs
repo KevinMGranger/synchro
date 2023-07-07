7dfe823d0211dd65259971f2d8a55dfd086c4c62
Kevin M Granger <git@kmg.fyi>
2023-07-07 19:01:10 -0400

    dorkument

--- src/util/windows.rs
+++ src/util/windows.rs
@@ -15,12 +15,29 @@ use windows::{
     },
 };
 
+/// A struct that is dynamically sized from an API call.
+///
+/// Several Windows APIs will dynamically fill in a byte
+/// buffer with a different struct's data, depending
+/// upon some dynamic type argument.
+/// This is a safe(ish) abstraction over such APIs.
 pub(crate) struct DynamicBufPtr<T> {
     buf: Vec<u8>,
     r#type: PhantomData<T>,
 }
 
 impl<T> DynamicBufPtr<T> {
+    /// Provide a closure over the API which takes:
+    /// - the pointer to the buffer to fill in
+    /// - the size of that buffer
+    /// - a pointer to the "required size" parameter
+    ///
+    /// ...and the DynamicBufPtr will call the API
+    /// with a null pointer to find the necessary size,
+    /// allocate that memory, and then call it again to be filled.
+    ///
+    /// Any errors from the first call _except_ ERROR_INSUFFICIENT_BUFFER
+    /// will be returned, as will any errors from the second call.
     pub(crate) fn new(
         mut f: impl FnMut(Option<*mut c_void>, u32, *mut u32) -> BOOL,
     ) -> WinResult<Self> {
@@ -70,6 +87,8 @@ impl<T> DerefMut for DynamicBufPtr<T> {
     }
 }
 
+/// A wrapper over PWSTRs that are meant to be
+/// freed with `LocalFree`.
 pub(crate) struct LocalPWSTR(PWSTR);
 
 impl Drop for LocalPWSTR {
