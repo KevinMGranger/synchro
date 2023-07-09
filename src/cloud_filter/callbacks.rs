e8e63ce01bc2bb93c232572cf6850f4810913cf9
Kevin M Granger <git@kevinmgranger.me>
2023-07-09 07:37:03 -0400

    start making safer callback interface

--- src/cloud_filter/callbacks.rs
+++ src/cloud_filter/callbacks.rs
@@ -1,3 +1,7 @@
+use std::{ffi::c_void, marker::PhantomData};
+
+use crate::util::windows::prelude::*;
+
 use windows::{
     core::{Result as WinResult, PCWSTR},
     Win32::Storage::CloudFilters::{
@@ -41,6 +45,44 @@ pub(crate) unsafe fn connect_callbacks(
     )
 }
 
-pub(crate) CallbackInfo<T> {
-    
+pub(crate) struct CallbackVolumeInfo {
+    guid_name: PCWSTR,
+    dos_name: PCWSTR,
+    pub(crate) serial_number: u32,
+}
+
+impl CallbackVolumeInfo {
+    fn guid_name(&self) -> TrustedBorrowedCWSTR<'_> {
+        unsafe { TrustedBorrowedCWSTR::from_raw(self.guid_name) }
+    }
+
+    fn dos_name(&self) -> TrustedBorrowedCWSTR<'_> {
+        unsafe { TrustedBorrowedCWSTR::from_raw(self.dos_name) }
+    }
+}
+
+pub(crate) struct SyncRootInfo<T> {
+    pub(crate) file_id: i64,
+    identity: *const c_void,
+    identity_length: u32,
+    identity_type: PhantomData<T>,
+}
+
+impl<T> SyncRootInfo<T>
+where
+    T: FromVoid,
+{
+    fn identity(&self) -> T {
+        unsafe {
+            let slice = std::slice::from_raw_parts(self.identity, self.identity_length as usize);
+            T::from_void_slice(slice)
+        }
+    }
+}
+
+pub(crate) struct CallbackInfo<T> {
+    connection_key: CF_CONNECTION_KEY,
+    context: *mut c_void, // TODO keep private
+
+    _d: PhantomData<T>,
 }
